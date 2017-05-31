a8e386afcf650b003867785edf1cb373830b7b65
Zen <cypryjefhardy@gmail.com>
2017-05-31 17:17:15 +0300

    setup main view menu animations

--- app/src/main/java/com/licenta/mihai/givemebook_android/MainActivity.java
+++ app/src/main/java/com/licenta/mihai/givemebook_android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.licenta.mihai.givemebook_android;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,10 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,14 +31,25 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.main_view_profile_friends)
     ImageView profileFriends;
 
-    @BindView(R.id.main_view_profile_settings)
-    ImageView profileSettings;
+    @BindView(R.id.main_view_profile_settings_layout)
+    RelativeLayout profileSettings;
 
     @BindView(R.id.main_view_search)
     ImageView profileSearch;
 
     @BindView(R.id.main_view_gridMainData)
     GridView gridView;
+
+
+    @BindView(R.id.main_view_profile_settings1)
+    ImageView settings1;
+
+    @BindView(R.id.main_view_profile_settings2)
+    ImageView settings2;
+
+    @BindView(R.id.main_view_search_input)
+    EditText searchInput;
+
 
     //endregion
 
@@ -55,7 +69,9 @@ public class MainActivity extends AppCompatActivity {
             "U", "V", "W", "X", "Y", "Z"};
 
 
-    private Boolean toogleMenu = true;
+    private Boolean toggleMenu = true;
+    private Boolean toggleSettings = true;
+    private Boolean toggleSearch = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -87,77 +103,254 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void enterAnimation() {
-
-        ObjectAnimator searchAnim;
-        searchAnim = ObjectAnimator.ofFloat(profileSearch, "translationX", -250);
-        searchAnim.setDuration(600);
-        searchAnim.start();
-
-        ObjectAnimator settingAnimX;
-        settingAnimX = ObjectAnimator.ofFloat(profileFriends, "translationX", -150);
-        settingAnimX.setDuration(600);
-        settingAnimX.start();
-
-        ObjectAnimator settingAnimY;
-        settingAnimY = ObjectAnimator.ofFloat(profileFriends, "translationY", 100);
-        settingAnimY.setDuration(600);
-        settingAnimY.start();
-
-
-        ObjectAnimator friendsAnim;
-        friendsAnim = ObjectAnimator.ofFloat(profileSettings, "translationY", 250);
-        friendsAnim.setDuration(600);
-        friendsAnim.start();
-    }
-
-    private void exitAnimation() {
-        ObjectAnimator searchAnim;
-        searchAnim = ObjectAnimator.ofFloat(profileSearch, "translationX", 250);
-        searchAnim.setDuration(600);
-        searchAnim.start();
-
-        ObjectAnimator settingAnimX;
-        settingAnimX = ObjectAnimator.ofFloat(profileFriends, "translationX", 150);
-        settingAnimX.setDuration(600);
-        settingAnimX.start();
-
-        ObjectAnimator settingAnimY;
-        settingAnimY = ObjectAnimator.ofFloat(profileFriends, "translationY", -100);
-        settingAnimY.setDuration(600);
-        settingAnimY.start();
-
-
-        ObjectAnimator friendsAnim;
-        friendsAnim = ObjectAnimator.ofFloat(profileSettings, "translationY", -250);
-        friendsAnim.setDuration(600);
-        friendsAnim.start();
-    }
 
     @OnClick({R.id.main_view_profile_picture, R.id.main_view_profile_friends, R.id.main_view_profile_settings, R.id.main_view_search})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.main_view_profile_picture:
-                if (toogleMenu) {
-                    enterAnimation();
-                    toogleMenu = false;
-                } else {
-                    exitAnimation();
-                    toogleMenu = true;
-                }
+                doMenuAnimations();
                 break;
             case R.id.main_view_profile_settings:
-                Toast.makeText(MainActivity.this, "settings", Toast.LENGTH_SHORT).show();
+                doSettingsAnimations();
                 break;
             case R.id.main_view_search:
-                Toast.makeText(getApplicationContext(),
-                        "search", Toast.LENGTH_SHORT).show();
+                doSearchAnimations();
                 break;
             case R.id.main_view_profile_friends:
-                Toast.makeText(getApplicationContext(),
-                       "fiends", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
 
+    //region menu animations
+    private void enterAnimation() {
+
+        profileSearch.animate().translationX(-250).setDuration(600);
+        ObjectAnimator friendsAnimX;
+        friendsAnimX = ObjectAnimator.ofFloat(profileFriends, "translationX", -150);
+        friendsAnimX.setDuration(600);
+        friendsAnimX.start();
+        ObjectAnimator friendsAnimY;
+        friendsAnimY = ObjectAnimator.ofFloat(profileFriends, "translationY", 100);
+        friendsAnimY.setDuration(600);
+        friendsAnimY.start();
+        profileSettings.animate().translationY(250).setDuration(600);
+    }
+
+    private void exitAnimation() {
+        profileSearch.animate().translationX(0).setDuration(600);
+        ObjectAnimator friendsAnimX;
+        friendsAnimX = ObjectAnimator.ofFloat(profileFriends, "translationX", 150);
+        friendsAnimX.setDuration(600);
+        friendsAnimX.start();
+
+        ObjectAnimator friendsAnimY;
+        friendsAnimY = ObjectAnimator.ofFloat(profileFriends, "translationY", -100);
+        friendsAnimY.setDuration(600);
+        friendsAnimY.start();
+        profileSettings.animate().translationY(0).setDuration(600);
+
+    }
+
+
+    private void enterSettingsAnimation() {
+        settings1.setVisibility(View.VISIBLE);
+        settings2.setVisibility(View.VISIBLE);
+        settings1.animate().translationX(-150).setDuration(600);
+        settings1.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                settings1.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        settings2.animate().translationY(150).setDuration(600);
+        settings2.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                settings2.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    private void exitSettingsAnimation() {
+        settings1.animate().translationX(0).setDuration(600);
+        settings1.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                settings1.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        settings2.animate().translationY(0).setDuration(600);
+        settings2.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                settings2.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+
+    private void doMenuAnimations() {
+        if (toggleMenu) {
+            enterAnimation();
+            toggleMenu = false;
+        } else {
+            resetAnimations();
+        }
+    }
+
+    private void doSettingsAnimations() {
+        if (toggleSettings) {
+            enterSettingsAnimation();
+            toggleSettings = false;
+        } else {
+            exitSettingsAnimation();
+            toggleSettings = true;
+        }
+    }
+
+    private void doSearchAnimations() {
+        if (toggleSearch) {
+            searchInput.setVisibility(View.VISIBLE);
+            searchInput.animate().translationX(-320).setDuration(600);
+            searchInput.animate().setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            toggleSearch = false;
+        } else {
+            searchInput.animate().translationX(320).setDuration(600);
+            searchInput.animate().setListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    searchInput.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+            searchInput.setText("");
+            toggleSearch = true;
+        }
+    }
+
+    private void resetAnimations() {
+        exitSettingsAnimation();
+        toggleSettings = true;
+        exitAnimation();
+        searchInput.animate().translationX(320).setDuration(600);
+        searchInput.animate().setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                searchInput.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        searchInput.setText("");
+        toggleSearch = true;
+        toggleMenu = true;
+    }
+
+    //endregion
 }
